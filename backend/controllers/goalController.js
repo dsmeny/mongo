827cd16b056c42cfebd43cca58af3622468bc331
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-02-10 07:10:18 -0500

    updated controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -17,11 +17,25 @@ const postGoals = async(req, res) => {
 }
 
 const putGoals = async(req, res) => {
-    res.json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.json(updatedGoal)
 }
 
 const deleteGoals = async(req, res) => {
-    res.json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findByIdAndDelete(req.params.id)
+
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+    res.json(goal)
 }
 
 module.exports = {
